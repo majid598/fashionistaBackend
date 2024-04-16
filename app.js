51ff1549f76585cc78cd8afe14c65d02c6ac4b0a
majid598 <143610202+majid598@users.noreply.github.com>
2024-04-17 01:16:28 +0630

    Update app.js

--- app.js
+++ app.js
@@ -11,15 +11,7 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:4173",
-      process.env.CLIENT_URL,
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
+  cors()
 );
 app.use(cookieParser());
 
