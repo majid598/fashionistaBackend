2e368e9b7ce4fec2cae5f0bb9d73534892fb4919
majid598 <143610202+majid598@users.noreply.github.com>
2024-04-17 00:56:36 +0630

    updated

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:4173",
-      // process.env.CLIENT_URL,
+      process.env.CLIENT_URL,
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
