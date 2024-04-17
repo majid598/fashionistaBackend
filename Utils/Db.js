58c451d765f772a7151cd4fbfef468f0875ef2a7
majid598 <143610202+majid598@users.noreply.github.com>
2024-04-17 11:00:06 +0630

    Update Db.js

--- Utils/Db.js
+++ Utils/Db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDb = (uri) => {
   mongoose
-    .connect(uri, { dbName: "FASHIONISTA" })
+    .connect(uri, { dbName: "fashionista-ecommerce" })
     .then((data) => console.log(`DB Connected To: ${data.connection.host}`))
     .catch((err) => {
       throw err;
